5984f0deb8b7d61f7662ef1b9d398478fc80c68a
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-02-10 13:55:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $json = file_get_contents('https://json-anj.herokuapp.com/json-1.php');
 
 $data = json_decode($json,true);
-$list = $data['books'];
+$list = $data['book'];
 //$list = $data['books'][1];
 
 ?>
