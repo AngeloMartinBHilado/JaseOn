2580347f5f07d077d17fa1d1f6203f9db4ce87e0
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-02-10 13:17:16 +0800

    Update json-1.php

--- json-1.php
+++ json-1.php
@@ -2,8 +2,8 @@
 include 'config.php';
 include 'class.books.php';
 header('Content-Type: application/json');
-$books = new Books();
-$list=$Books->get_books();
+$books = new books();
+$list=$books->get_books();
 echo "{\"Books\":";
 echo json_encode($list);
 echo "}";
