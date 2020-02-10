4b0cb934f68f37f941000c85e0d14b6eb88ce07a
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-02-10 13:09:37 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$json = file_get_contents('https://json-activity.herokuapp.com/json-1.php');
+
+$data = json_decode($json,true);
+$list = $data['articles'];
+//$list = $data['pets'][1];
+
+?>
+
+<h1>Books</h1>
+
+<table border="1px">
+    <tr class="head">
+        <td>Title</td>
+        <td>Author</td>
+        <td>Genre</td>
+        <td>Date Created</td>
+    </tr>
+<?php
+foreach($list as $value){
+    ?>
+    <tr>
+        <td><?php echo $value['Title'];?></td>
+        <td><?php echo $value['Author'];?></td>
+        <td><?php echo $value['Genre'];?></td>
+        <td><?php echo $value['Date'];?></td>
+    </tr>
+<?php
+}
+?>
+</table>
